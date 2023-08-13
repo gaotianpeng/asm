32a9f1ff6fde59c38a7657c1c801f2cc1b9e76b5
gaotianpeng <gtpsydm@gmail.com>
2023-08-13 20:42:22 +0800

    rtc 01: print string

--- rtc/rtc.asm
+++ rtc/rtc.asm
@@ -1,298 +1,208 @@
-SECTION header vstart=0                     ;定义用户程序头部段
-    program_length  dd program_end          ;程序总长度[0x00]
+section header vstart=0
+        program_length          dd  program_end                             ; [0x00]
 
-    ;用户程序入口点
-    code_entry      dw start                ;偏移地址[0x04]
-                    dd section.code.start   ;段地址[0x06]
+        code_entry              dw  start                                   ; [0x04]
+                                dd  section.code.start                      ; [0x06]
 
-    realloc_tbl_len dw (header_end-realloc_begin)/4
-                                            ;段重定位表项个数[0x0a]
+        realloc_table_len       dw  (header_end - realloc_begin) / 4;       ; [0x0a]
 
     realloc_begin:
-    ;段重定位表
-    code_segment    dd section.code.start   ;[0x0c]
-    data_segment    dd section.data.start   ;[0x14]
-    stack_segment   dd section.stack.start  ;[0x1c]
+        code_segment            dd section.code.start                       ; [0x0c]
+        data_segment            dd section.data.start                       ; [0x14]
+        stack_segment           dd section.stack.start                      ; [0x1c]
 
 header_end:
 
-;===============================================================================
-SECTION code align=16 vstart=0           ;定义代码段（16字节对齐）
-new_int_0x70:
-      push ax
-      push bx
-      push cx
-      push dx
-      push es
+section code align=16 vstart=0
 
-  .w0:
-      mov al,0x0a                        ;阻断NMI。当然，通常是不必要的
-      or al,0x80
-      out 0x70,al
-      in al,0x71                         ;读寄存器A
-      test al,0x80                       ;测试第7位UIP
-      jnz .w0                            ;以上代码对于更新周期结束中断来说
-                                         ;是不必要的
-      xor al,al
-      or al,0x80
-      out 0x70,al
-      in al,0x71                         ;读RTC当前时间(秒)
-      push ax
-
-      mov al,2
-      or al,0x80
-      out 0x70,al
-      in al,0x71                         ;读RTC当前时间(分)
-      push ax
-
-      mov al,4
-      or al,0x80
-      out 0x70,al
-      in al,0x71                         ;读RTC当前时间(时)
-      push ax
-
-      mov al,0x0c                        ;寄存器C的索引。且开放NMI
-      out 0x70,al
-      in al,0x71                         ;读一下RTC的寄存器C，否则只发生一次中断
-                                         ;此处不考虑闹钟和周期性中断的情况
-      mov ax,0xb800
-      mov es,ax
-
-      pop ax
-      call bcd_to_ascii
-      mov bx,12*160 + 36*2               ;从屏幕上的12行36列开始显示
-
-      mov [es:bx],ah
-      mov [es:bx+2],al                   ;显示两位小时数字
-
-      mov al,':'
-      mov [es:bx+4],al                   ;显示分隔符':'
-      not byte [es:bx+5]                 ;反转显示属性
-
-      pop ax
-      call bcd_to_ascii
-      mov [es:bx+6],ah
-      mov [es:bx+8],al                   ;显示两位分钟数字
-
-      mov al,':'
-      mov [es:bx+10],al                  ;显示分隔符':'
-      not byte [es:bx+11]                ;反转显示属性
-
-      pop ax
-      call bcd_to_ascii
-      mov [es:bx+12],ah
-      mov [es:bx+14],al                  ;显示两位小时数字
-
-      mov al,0x20                        ;中断结束命令EOI
-      out 0xa0,al                        ;向从片发送
-      out 0x20,al                        ;向主片发送
-
-      pop es
-      pop dx
-      pop cx
-      pop bx
-      pop ax
-
-      iret
-
-;-------------------------------------------------------------------------------
-bcd_to_ascii:                            ;BCD码转ASCII
-                                         ;输入：AL=bcd码
-                                         ;输出：AX=ascii
-      mov ah,al                          ;分拆成两个数字
-      and al,0x0f                        ;仅保留低4位
-      add al,0x30                        ;转换成ASCII
-
-      shr ah,4                           ;逻辑右移4位
-      and ah,0x0f
-      add ah,0x30
-
-      ret
-
-;-------------------------------------------------------------------------------
 start:
-      mov ax,[stack_segment]
-      mov ss,ax
-      mov sp,ss_pointer
-      mov ax,[data_segment]
-      mov ds,ax
+    mov ax, [stack_segment]
+    mov ss, ax
+    mov sp, ss_pointer
+    mov ax, [data_segment]
+    mov ds, ax
 
-      mov bx,init_msg                    ;显示初始信息
-      call put_string
+    mov bx, init_msg                ; 显示初始信息
+    call put_string
+    mov bx, inst_msg                ; 显示安装信息
+    call put_string
 
-      mov bx,inst_msg                    ;显示安装信息
-      call put_string
+    jmp $
 
-      mov al,0x70
-      mov bl,4
-      mul bl                             ;计算0x70号中断在IVT中的偏移
-      mov bx,ax
+; 显示字符串(0结尾)
+; 输入 DS:BX=串地址
+put_string:
+        mov cl, [bx]
+        cmp cl, 0
+        je .exit
+        call put_char
+        inc bx
+        jmp put_string
 
-      cli                                ;防止改动期间发生新的0x70号中断
+    .exit:
+        ret
 
-      push es
-      mov ax,0x0000
-      mov es,ax
-      mov word [es:bx],new_int_0x70      ;偏移地址。
+; 显示一个字符
+; 输入: cl=字符的ascii
+put_char:
+    push ax
+    push bx
+    push cx
+    push dx
+    push ds
+    push es
 
-      mov word [es:bx+2],cs              ;段地址
-      pop es
+    ; 获取当前光标位置, 两个光标寄存器，索引分别是14, 15, 分别用于提供光标位置的高8位和低8位
+    mov dx, 0x3d4       ; 端口0x3d4用于光标操作
+    mov al, 14          ; 命令字节，指定高字节光标位置
+    out dx, al          ; 发送命令字节到端口
+    mov dx, 0x3d5       ; 端口0x3d5用于读取数据
+    in al, dx           ; 读取高8位
+    mov ah, al          ; 将光标高字节保存到ah寄存器
 
-      mov al,0x0b                        ;RTC寄存器B
-      or al,0x80                         ;阻断NMI
-      out 0x70,al
-      mov al,0x12                        ;设置寄存器B，禁止周期性中断，开放更
-      out 0x71,al                        ;新结束后中断，BCD码，24小时制
+    mov dx, 0x3d4
+    mov al, 15          ; 命令字节，指定低字节光标位置
+    out dx, al
+    mov dx, 0x3d5
+    in al, dx           ; 读光标低字节
+    mov bx, ax          ; 将光标位置的16位值保存到bx寄存器
 
-      mov al,0x0c
-      out 0x70,al
-      in al,0x71                         ;读RTC寄存器C，复位未决的中断状态
+    cmp cl, 0x0d        ; 是否是回车
+    jnz .put_0a         ; 不是
+    mov ax, bx          ; 将光标位置复制到ax寄存器
+    mov bl, 80          ; 一行有80个字符
+    div bl              ; 将光标位置除以80，得到行号
+    mul bl              ; 将行号每次以80，得到行的起始位置
+    mov bx, ax          ; 将行起始位置保存到bx寄存器
+    jmp .set_cursor     ; 跳转到设置光标位置的代码处
 
-      in al,0xa1                         ;读8259从片的IMR寄存器
-      and al,0xfe                        ;清除bit 0(此位连接RTC)
-      out 0xa1,al                        ;写回此寄存器
 
-      sti                                ;重新开放中断
+.put_0a:
+    cmp cl, 0x0a        ; 换行符？
+    jnz .put_ch         ; 不是，正常显示字符
+    add bx, 80          ; 增加光标位置，将光标移到下一行行首
+    jmp .roll_screen    ; 处理滚屏操作
 
-      mov bx,done_msg                    ;显示安装完成信息
-      call put_string
 
-      mov bx,tips_msg                    ;显示提示信息
-      call put_string
+; 正常显示字符
+.put_ch:
+    mov ax, 0xb800
+    mov es, ax
+    shl bx, 1           ; 将光标位置每次以2，每个字符占两个字节
+    mov [es:bx], cl     ; 将字符写入显存
 
-      mov cx,0xb800
-      mov ds,cx
-      mov byte [12*160 + 33*2],'@'       ;屏幕第12行，35列
+    ; 推进光标位置到下一个字符
+    shr bx, 1           ; 将光标位置右移1位, 除以2
+    add bx, 1           ; 增加光标位置，指向下一个字符位置
 
- .idle:
-      hlt                                ;使CPU进入低功耗状态，直到用中断唤醒
-      not byte [12*160 + 33*2+1]         ;反转显示属性
-      jmp .idle
+.roll_screen:
+    cmp bx, 2000        ; 光标超出屏幕?
+    jl .set_cursor      ; 如果没有，跳转到设置光标位置的代码块
 
-;-------------------------------------------------------------------------------
-put_string:                              ;显示串(0结尾)。
-                                         ;输入：DS:BX=串地址
-         mov cl,[bx]
-         or cl,cl                        ;cl=0 ?
-         jz .exit                        ;是的，返回主程序
-         call put_char
-         inc bx                          ;下一个字符
-         jmp put_string
+    ; 滚屏操作
+    mov ax, 0xB800
+    mov ds, ax
+    mov es, ax
+    cld                 ; 清除方向标志，用于字符串传输指令
+    mov si, 0xa0        ; 源偏移，从第一行之外开始复制
+    mov di, 0x00        ; 目标偏移，从第一行开始
+    mov cx, 1920        ; 复制字符的总数量(80*24)
+    rep movsw           ; 重复执行movsw指令，复制数据
+    mov bx, 3840        ; 清屏屏幕底部一行的偏移位置
+    mov cx, 80          ; 一行有80个字符
 
-   .exit:
-         ret
+.cls:
+    mov word [es:bx], 0x0720        ; 将空白字符写入显存，以清除一行
+    add bx, 2           ; 增加偏移以处理下一字符
+    loop .cls           ; 循环处理直到下一行清除完成
 
-;-------------------------------------------------------------------------------
-put_char:                                ;显示一个字符
-                                         ;输入：cl=字符ascii
-         push ax
-         push bx
-         push cx
-         push dx
-         push ds
-         push es
+    mov bx, 1920        ; 将光标位置设置为新的屏幕底部
 
-         ;以下取当前光标位置
-         mov dx,0x3d4
-         mov al,0x0e
-         out dx,al
-         mov dx,0x3d5
-         in al,dx                        ;高8位
-         mov ah,al
+.set_cursor:
+    ; 更新光标位置
+    mov dx, 0x3d4       ; 设置光标位置的端口
+    mov al, 14          ; 命令字节，高字节光标位置
+    out dx, al
+    mov dx, 0x3d5       ; 数据传输端口
+    mov al, bh          ; 将光标高字节写入数据端口
+    out dx, al
+    mov dx, 0x3d4
+    mov al, 15          ; 命令字节，低字节光标位置
+    out dx, al          ; 发送命令到端口
+    mov dx, 0x3d5       ; 数据传输端口
+    mov al, bl          ; 将光标的低字节（bl）写入数据端口
+    out dx, al          ; 将光标低字节写入数据端口
 
-         mov dx,0x3d4
-         mov al,0x0f
-         out dx,al
-         mov dx,0x3d5
-         in al,dx                        ;低8位
-         mov bx,ax                       ;BX=代表光标位置的16位数
+    pop es
+    pop ds
+    pop dx
+    pop cx
+    pop bx
+    pop ax
 
-         cmp cl,0x0d                     ;回车符？
-         jnz .put_0a                     ;不是。看看是不是换行等字符
-         mov ax,bx                       ;
-         mov bl,80
-         div bl
-         mul bl
-         mov bx,ax
-         jmp .set_cursor
+    ret
 
- .put_0a:
-         cmp cl,0x0a                     ;换行符？
-         jnz .put_other                  ;不是，那就正常显示字符
-         add bx,80
-         jmp .roll_screen
+section data align=16 vstart=0
+    init_msg            db 'Starting...', 0x0d, 0x0a, 0
+    inst_msg            db 'Installing a new interrupt 70H...', 0
+    done_msg            db 'Done.', 0x0d, 0x0a, 0
+    tips_msg            db 'Clock is now working.', 0
 
- .put_other:                             ;正常显示字符
-         mov ax,0xb800
-         mov es,ax
-         shl bx,1
-         mov [es:bx],cl
 
-         ;以下将光标位置推进一个字符
-         shr bx,1
-         add bx,1
-
- .roll_screen:
-         cmp bx,2000                     ;光标超出屏幕？滚屏
-         jl .set_cursor
-
-         mov ax,0xb800
-         mov ds,ax
-         mov es,ax
-         cld
-         mov si,0xa0
-         mov di,0x00
-         mov cx,1920
-         rep movsw
-         mov bx,3840                     ;清除屏幕最底一行
-         mov cx,80
- .cls:
-         mov word[es:bx],0x0720
-         add bx,2
-         loop .cls
-
-         mov bx,1920
-
- .set_cursor:
-         mov dx,0x3d4
-         mov al,0x0e
-         out dx,al
-         mov dx,0x3d5
-         mov al,bh
-         out dx,al
-         mov dx,0x3d4
-         mov al,0x0f
-         out dx,al
-         mov dx,0x3d5
-         mov al,bl
-         out dx,al
-
-         pop es
-         pop ds
-         pop dx
-         pop cx
-         pop bx
-         pop ax
-
-         ret
-
-;===============================================================================
-SECTION data align=16 vstart=0
-
-    init_msg       db 'Starting...',0x0d,0x0a,0
-
-    inst_msg       db 'Installing a new interrupt 70H...',0
-
-    done_msg       db 'Done.',0x0d,0x0a,0
-
-    tips_msg       db 'Clock is now working.',0
-
-;===============================================================================
-SECTION stack align=16 vstart=0
-
-                 resb 256
+section stack align=16 vstart=0
+        resb 256
 ss_pointer:
 
-;===============================================================================
-SECTION program_trail
+section program_trail
 program_end:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
