fc7310c7ff3363da8a4c664ba3f443a7fbaf4d78
gaotianpeng <gtpsydm@gmail.com>
2023-08-13 22:15:56 +0800

    rtc with comment

--- rtc/rtc.asm
+++ rtc/rtc.asm
@@ -14,7 +14,105 @@ section header vstart=0
 header_end:
 
 section code align=16 vstart=0
+; 主要功能是在屏幕上显示实时时钟的小时、分钟和秒
+; 代码首先等待RTC更新结束（等待UIP位为0），然后依次读取RTC的秒、分、时并转换为ASCII码后显示在屏幕上
+new_int_0x70:
+        push ax
+        push bx
+        push cx
+        push dx
+        push es
 
+    .w0:
+        mov al, 0x0a                ; 将0x0a（10）写入RTC的索引寄存器
+        or al, 0x80                 ; 设置UIP位（RTC更新标志）为1，阻断NMI
+        out 0x70, al                ; 发送命令到RTC到索引寄存器
+        in al, 0x71                 ; 读取RTC状态寄存器A
+        test al, 0x80               ; 测试第7位UIP是否为1, 即RTC是否正在更新
+        jnz .w0                     ; 如果UIP为1, 说明RTC还在更新，继续等待
+
+        ; 读取RTC的秒、分、时
+        xor al, al
+        or al, 0x80                 ; 设置UIP位为1，阻断NMI
+        out 0x70, al                ; 发送命令到RTC的索引寄存器
+        in al, 0x71                 ; 读取RTC的秒寄存器
+        push ax                     ; 保存读取的秒
+
+        mov al, 2                   ; 将2号写入RTC的索引寄存器，准备读取RTC的分
+        or al, 0x80
+        out 0x70, al
+        in al, 0x71                 ; 读取RTC的分寄存器
+        push ax                     ; 保存读取的分
+
+        mov al, 4                   ; 将4号写入RTC的索引寄存器，准备读取RTC的时
+        or al, 0x80
+        out 0x70, al
+        in al, 0x71                 ; 读取RTC的时寄存器
+        push ax                     ; 保存读取的时
+
+        mov al, 0x0c                ; 将0x0c写入RTC的索引寄存器，以准备读取寄存器C
+        out 0x70, al
+        in al, 0x71                 ; 读取RTC的寄存器C，确保中断只发生一次
+
+        mov ax, 0xb800
+        mov es, ax
+
+        ; 显示读取的时、分、钞
+        pop ax                      ; 恢复读取的时
+        call bcd_to_ascii
+        mov bx, 12*160 + 36*2       ; 屏幕上的位置
+
+        mov [es:bx], ah             ; 显示小时的高位
+        mov [es:bx + 2], al         ; 显示小时的低位
+
+        mov al, ':'                 ; 显示分隔符
+        mov [es:bx + 4], al
+
+        not byte [es:bx + 5]        ; 反转显示属性(颜色)
+
+        pop ax                      ; 恢复读取的分
+        call bcd_to_ascii
+        mov [es:bx + 6], ah         ; 显示分钟的高位
+        mov [es:bx + 8], al         ; 显示分钟的低位
+
+        mov al, ':'                 ; 显示分隔符
+        mov [es:bx + 10], al
+
+        not byte [es:bx + 11]       ; 反转显示属性(颜色)
+
+        pop ax                      ; 恢复读取的秒
+        call bcd_to_ascii
+        mov [es:bx + 12], ah        ; 显示秒的高位
+        mov [es:bx + 14], al        ; 显示秒的低位
+
+        mov al, 0x20                ; 中断结束命令EOI
+        out 0xa0, al                ; 向从片发送EOI
+        out 0x20, al                ; 向主片发送EOI
+
+        pop es
+        pop dx
+        pop cx
+        pop bx
+        pop ax
+
+        iret
+
+bcd_to_ascii:
+    mov ah, al       ; 复制al到ah寄存器，备份低4位
+    and al, 0x0f      ; 将al的值与0x0f进行按位与操作，仅保留低4位（个位）
+    add al, 0x30      ; 将低4位的值加上0x30，转换成ASCII码
+
+    shr ah, 4         ; 将ah寄存器的值逻辑右移4位，获取高4位的值（十位）
+    and ah, 0x0f      ; 将ah的值与0x0f进行按位与操作，仅保留低4位（十位）
+    add ah, 0x30      ; 将低4位的值加上0x30，转换成ASCII码（十位）
+
+    ret               ; 返回，将转换后的ASCII码保存在AX寄存器中
+
+; BCD（Binary-Coded Decimal）码是一种用二进制数来表示十进制数字的编码方式。
+; 在BCD编码中，每个十进制数位用4位二进制数来表示，通常是将十进制的0到9每个数字分别编码为4位二进制，从0000到1001。
+; 这种编码方式使得每个BCD码可以直接对应一个十进制数字，便于数字的处理和转换
+; 将一个BCD（二进制码十进制）格式的数值转换为ASCII码表示，分别转换十位和个位的数字，
+; 并将结果存储在AH和AL寄存器中。函数最后通过ret指令返回，此时AH中存储着十位的ASCII码，AL中存储着个位的ASCII码
 start:
     mov ax, [stack_segment]
     mov ss, ax
@@ -27,7 +125,53 @@ start:
     mov bx, inst_msg                ; 显示安装信息
     call put_string
 
-    jmp $
+    ; 计算0x70号中断在IVT（中断向量表）中的偏移地址
+    mov al, 0x70
+    mov bl, 4
+    mul bl                          ; 将al乘以4，计算偏移
+    mov bx, ax                      ; 将计算的偏移保存到bx
+
+
+    cli                             ; 禁用中断，防止改动期间发生新的 0x70 号中断
+
+    push es
+    mov ax, 0x0000
+    mov es, ax                      ; 设置es寄存器为0，用于访问IVT
+    mov word [es:bx], new_int_0x70  ; 设置中断处理程序的偏移地址
+    mov word [es: bx + 2], cs       ; 设置中断处理程序的段地址
+    pop es
+
+    mov al, 0x0b                    ; 设置RTC寄存器B
+    or al, 0x80                     ; 阻断NMI
+    out 0x70, al
+
+    mov al, 0x12                    ; 设置寄存器B，禁止周期性中断，开放新结束后中断，BCD码，24小时制
+    out 0x71, al
+
+    mov al, 0x0c                    ; 设置RTC寄存器C
+    out 0x70, al
+    in al, 0x71                     ; 读RTC寄存器C，复位未决的中断状态
+
+    in al, 0xa1                     ; 读8259 从片的IMR寄存器
+    and al, 0xfe                    ; 清除bit 0
+    out 0xa1, al                    ; 将修改后的IMR寄存器写回
+
+    sti                             ; 放开中断
+
+    mov bx, done_msg                ; 显示安装完成信息
+    call put_string
+
+    mov bx, tips_msg                ; 显示提示信息
+    call put_string
+
+    mov cx, 0xb800
+    mov ds, cx
+    mov byte [12*160 + 33*2], '@'
+
+.idle:
+    hlt                              ; 使CPU进入低功耗状态，直到有中断唤醒
+    not byte [12*160 + 33*2 + 1]
+    jmp .idle
 
 ; 显示字符串(0结尾)
 ; 输入 DS:BX=串地址
